2b2fc74d1ac3de6a123ef33063b447af45c151b7
ZachNg03 <111360360+ZachNg03@users.noreply.github.com>
2024-09-16 16:34:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee1.cnkuuf8qw5qy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "ngxuxiang-employee1"
+custombucket = "ngxuxiang-employee"
 customregion = "us-east-1b"
 
